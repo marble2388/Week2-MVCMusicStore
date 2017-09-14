c90c4967b1e400d5d6f6a9d29bb7ca9214c7788a
Jeff Hall <jeffahall94@gmail.com>
2017-09-14 12:21:44 -0400

    Basic Set Up
    ASP.NET
    MVC
    .gitignore file

---
+++ Week2-MVCMusicStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week2_MVCMusicStore.Startup))]
+namespace Week2_MVCMusicStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
